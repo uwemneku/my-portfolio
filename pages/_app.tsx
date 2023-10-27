354d75f669410a3059885557d41cd6def03af1e8
uwem isarel <iamuwem@gmail.com>
2023-10-27 15:34:42 +0100

    updated dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Header } from "../components";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
